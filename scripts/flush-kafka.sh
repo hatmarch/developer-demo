a5c33cb97f41beffe516f79a2aa429e7d7c16631
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-10-11 06:02:54 +0000

    Tekton resources appropriate for 4.5

---
+++ scripts/flush-kafka.sh
@@ -0,0 +1,2 @@
+# NOTE: would be better to set a shorter default retention policy or set on topics
+# See: https://www.allprogrammingtutorials.com/tutorials/configuring-messages-retention-time-in-kafka.php
